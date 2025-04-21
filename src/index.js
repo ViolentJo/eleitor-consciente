f038fe74b8147d4ef104b6d0d90d4eec26902a2d
ViolentJorge <jorge3dart@gmail.com>
2025-04-21 17:17:11 +0100

    Primeiro commit do projeto Eleitor Consciente

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import EleitorConsciente from './EleitorConsciente';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <EleitorConsciente />
+  </React.StrictMode>
+);
